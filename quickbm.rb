69c9ab021d1742758584bce48924f549420533dd
Toshiyuki Masui <masui@pitecan.com>
2018-05-11 05:18:19 +0900

    英数字以外の名前を許さないようにする

--- quickbm.rb
+++ quickbm.rb
@@ -63,13 +63,18 @@ end
 
 get '/:name!' do |shortname|
   getcookie
-  data = $bmdb.find({hash: @hash, shortname: shortname}).limit(1).first
-  if data
-    @shortname = shortname
-    @description = data['description']
-    @longname = data['longname']
+  if shortname =~ /^[a-zA-Z0-9_\-]+$/
+    data = $bmdb.find({hash: @hash, shortname: shortname}).limit(1).first
+    if data
+      @shortname = shortname
+      @description = data['description']
+      @longname = data['longname']
+    end
+    erb :edit
+  else 
+    @data = $bmdb.find({hash: @hash})
+    erb :list
   end
-  erb :edit
 end
 
 post '/' do # ログインフォームから
@@ -89,8 +94,12 @@ end
 get '/*' do
   shortname = params['splat'].join('/') # a/b みたいなのを許す
   getcookie
-  data = $bmdb.find({hash: @hash, shortname: shortname}).limit(1).first
 
+  unless shortname =~ /^[a-zA-Z0-9_\-]+$/
+    redirect "https://www.google.com/search?q=#{shortname}"
+  end
+
+  data = $bmdb.find({hash: @hash, shortname: shortname}).limit(1).first
   if request.env['HTTP_REFERER'].to_s.include?(request.env['HTTP_HOST'])
     if data
       @shortname = shortname
@@ -102,7 +111,7 @@ get '/*' do
     if data then
       redirect data['longname']
     else
-      redirect "https://www.google.com/search?q=#{CGI.escape(shortname)}"
+      redirect "https://www.google.com/search?q=#{shortname}"
       # redirect "https://www.google.com/search?q=#{URI.encode{shortname}}"
     end
   end
